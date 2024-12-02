b422b9479a1df9695e3e50a2ae9495e24a0488de
Jules G. <jules.grdn@gmail.com>
2024-12-02 15:04:23 +0100

    Ajout des 3 premiers exercices

--- index.js
+++ index.js
@@ -61,9 +61,84 @@ app.set("views", path.join(__dirname, "views")); // Les templates sont dans le d
 hbs.registerPartials(path.join(__dirname, "views", "partials")); // Permet d'utiliser les partials
 
 app.use(express.static("public"));
+app.use(express.json()); // Analyse les requêtes JSON
+app.use(express.urlencoded({ extended: true })); // Analyse les données encodées dans le corps des requêtes
 
-app.get("/games/create", (req, res) => {
-    res.status(200).render("games/create");
+// Affichage de la liste de tous les jeux
+app.get("/games", async (req, res) => {
+    try {
+        const games = await prisma.game.findMany();
+        res.status(200).render("games/index", { games });
+    } catch (error) {
+        console.error(error);
+        res.status(500).render("error", {error: "Une erreur est survenue."});
+    }
+});
+
+app.get("/games/:id", async (req, res) => {
+    try {
+        const game = await prisma.game.findUnique({
+            where: { id: parseInt(req.params.id) },
+        });
+        res.status(200).render("games/details", { game });
+    } catch (err) {
+        console.error(err);
+        res.status(500).render("error", {error: "Une erreur est survenue."});
+    }
+})
+
+// Création d'un jeu : frontend
+app.get("/games/create", async (req, res) => {
+    try {
+        const genres = await prisma.genre.findMany();
+        const editor = await prisma.editor.findMany();
+        res.status(200).render("games/create", {genres, editor});
+    } catch (err) {
+        console.error(err);
+        res.status(500).render("error", {error: "Une erreur est survenue."});
+    }
+});
+
+// Création d'un jeu : backend
+app.post("/games/create", async (req, res) => {
+    try {
+        const gameData = req.body;
+
+        const genreExists = await prisma.genre.findUnique({
+            where: { id: parseInt(gameData.genre) },
+        });
+
+        if (!genreExists) {
+            throw new Error("Le genre spécifié n'existe pas.");
+        }
+
+        const editorExists = await prisma.editor.findUnique({
+            where: { id: parseInt(gameData.editor) },
+        });
+
+        if (!editorExists) {
+            throw new Error("L'éditeur spécifié n'existe pas.");
+        }
+
+        const game = await prisma.game.create({
+            data: {
+                title: gameData.title,
+                description: gameData.description,
+                releaseDate: new Date(gameData.date),
+                genre: {
+                    connect: { id: parseInt(gameData.genre) }
+                },
+                editor: {
+                    connect: { id: parseInt(gameData.editor) }
+                }
+            },
+        });
+
+        res.status(200).render("games/create");
+    } catch (err) {
+        console.log(err);
+        res.status(500).render("error", {error: "Une erreur est survenue."});
+    }
 });
 
 app.use((err, req, res, next) => {
@@ -73,7 +148,7 @@ app.use((err, req, res, next) => {
 
 startServer()
     .then((req) => {
-        console.log(req);
+        console.log("Starting server...");
     })
     .catch((err) => {
         console.error("Error during server initialization:", err);
